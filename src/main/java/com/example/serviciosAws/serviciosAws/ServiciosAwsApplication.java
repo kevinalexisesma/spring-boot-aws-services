50617a2eb5a4bab78d0c91c706576d4992b4c179
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-17 14:29:57 -0500

    feat: First commit

---
+++ src/main/java/com/example/serviciosAws/serviciosAws/ServiciosAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciosAws.serviciosAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serviciosAws/serviciosAws/ServiciosAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciosAws.serviciosAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
